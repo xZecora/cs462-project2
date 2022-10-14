9814ef534280246dbc081e72e41bdac7c38bf272
Bryant Collins <vitrial@vitrial.xyz>
2022-10-14 14:25:19 -0400

    create.sh

--- create.sh
+++ create.sh
@@ -4,29 +4,53 @@
 # Assignment 2 - Create
 # Purpose: Creates files that contain header information about an invoice.
 
-# tracking the exit status
-status=0
+# tracking the exit code
+code=0
+
+output=""
 
 # validating the arguments passed by the user
 if [[ $# != 2 ]]; then
   echo 'usage: create.sh -i|-o filename'
-  status=$(($status + 1))
+  code=$(($code+1))
 
-  exit $status
+  exit $code
 fi
 
 type=""
 
-# checking the flag entered by the user
-if [[ $1 == "-i" ]]
+if [[ $1 != "-"* && $2 != "-"* ]]
 then
-  type = ".iso"
-elif [[ $1 == "-o" ]]
+  echo 'error: needs flag'
+  code=$(($code+3))
+  exit $code
+elif [[ $1 == "-"* && $2 == "-"* ]]
 then
-  type = ".oso"
-else
-  echo 'error: invalid flag'
-  status=$(($status + 2))
-
-  exit $status
+  echo 'error: needs filename'
+  code=$(($code+4))
+  exit $code
 fi
+
+# checking the flag entered by the user
+for i in $@
+do
+  if [[ $i == "-"* ]]
+  then
+    if [[ $i == "-i" ]]
+    then
+      type=".iso"
+    elif [[ $i == "-o" ]]
+    then
+      type=".oso"
+    else
+      echo 'error: invalid flag'
+      code=$(($code+2))
+      exit $code
+    fi
+  else
+    output=$i
+  fi
+done
+
+output=$output$type
+echo $output

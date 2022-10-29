33c2452c5ce490e2835c828bb6b797a9076e5842
Brayan Mauricio-Gonzalez <bomauriciogonzalez1@catamount.wcu.edu>
2022-10-28 21:18:20 -0400

    fixed regexes

--- new_valid.sh
+++ new_valid.sh
@@ -50,9 +50,9 @@ fi
 
 # validating the address header line
 address=$(grep "[^[:space:]]" $1 | head -2 | tail -1)
-if [[ $(echo $address | grep "address:[0-9]+ [A-Za-z]+, [A-Za-z]+, [A-Za-z]+") == '' ]]
+if [[ $(echo -n $address | grep -E "address:[0-9]+ [A-Za-z]+( [A-Za-z]+)*, [A-Za-z]+( [A-Za-z]+)*, [A-Za-z]+") == '' ]]
 then
-  if [[ $address == '' ]]
+  if [[ $address == $customer ]]
   then
     echo -e "ERROR: Missing header line\nLast header line: $customer"
   else
@@ -62,16 +62,16 @@ then
 fi
 
 # checking state length
-if [[ $(echo $address | awk -F, '{print $3}' | tr -d "\n " | wc -c) != 2 ]]
+if [[ $(echo -n $address | awk -F, '{print $3}' | tr -d "\n " | wc -c) -ne 2 ]]
 then
-  echo "'$(echo $address | awk -F, '{print $3}' | tr -d "\n ")' is the wrong length for a state abbreviation"
+  echo "'$(echo -n $address | awk -F, '{print $3}' | tr -d "\n ")' is the wrong length for a state abbreviation"
   exit 4
 fi
 
 # checking state for in-state vs out-of-state
 if [[ $1 == *'.iso' ]]
 then
-  if [[ $(echo $address | awk -F, '{print $3}') != *'NC' ]]
+  if [[ $(echo -n $address | awk -F, '{print $3}') != *'NC' ]]
   then
     echo "ERROR: Invalid state for an iso file, must be 'NC'"
     exit 4
@@ -80,7 +80,7 @@ fi
 
 if [[ $1 == *'.oso' ]]
 then
-  if [[ $(echo $address | awk -F, '{print $3}') == *'NC' ]]
+  if [[ $(echo -n $address | awk -F, '{print $3}') == *'NC' ]]
   then
     echo "ERROR: Invalid state for an oso file, must be not be 'NC'"
     exit 4
@@ -89,37 +89,37 @@ fi
 
 # validating the categories header line
 categories=$(grep "[^[:space:]]" $1 | head -3 | tail -1)
-if [[ $(echo $categories | grep "categories:[^[:space:]]+(,[^[:space:]]+)*") == '' ]]
+if [[ $(echo $categories | grep -E "categories:[A-Za-z]+(,[A-Za-z]+)*$") == '' ]]
 then
-  if [[ $categories == '' ]]
+  if [[ $categories == $address ]]
   then
     echo -e "ERROR: Missing header line\nLast header line: $address"
   else
-    echo "ERROR: Invalid categories header line, must be of the form 'categories:<category>[,<category>]+'"
+    echo "ERROR: Invalid categories header line, must be of the form 'categories:<category>[,<category>]*'"
   fi
   exit 5
 fi
 
 # making sure no keywords are used as a category name
-if [[ $(echo $categories | awk -F: '{print $2}') == *[Cc][Uu][Ss][Tt][Oo][Mm][Ee][Rr]* ]]
+if [[ $(echo -n $categories | awk -F: '{print $2}') == *[Cc][Uu][Ss][Tt][Oo][Mm][Ee][Rr]* ]]
 then
   echo "ERROR: Cannot use the 'customer' keyword as category"
   exit 5
 fi
 
-if [[ $(echo $categories | awk -F: '{print $2}') == *[Aa][Dd][Dd][Rr][Ee][Ss][Ss]* ]]
+if [[ $(echo -n $categories | awk -F: '{print $2}') == *[Aa][Dd][Dd][Rr][Ee][Ss][Ss]* ]]
 then
   echo "ERROR: Cannot use 'address' keyword as a category"
   exit 5
 fi
 
-if [[ $(echo $categories | awk -F: '{print $2}') == *[Cc][Aa][Tt][Ee][Gg][Oo][Rr][Ii][Ee][Ss]* ]]
+if [[ $(echo -n $categories | awk -F: '{print $2}') == *[Cc][Aa][Tt][Ee][Gg][Oo][Rr][Ii][Ee][Ss]* ]]
 then
   echo "ERROR: Cannot use 'categories' keyword as a category"
   exit 5
 fi
 
-if [[ $(echo $categories | awk -F: '{print $2}') == *[Ii][Tt][Ee][Mm][Ss]* ]]
+if [[ $(echo -n $categories | awk -F: '{print $2}') == *[Ii][Tt][Ee][Mm][Ss]* ]]
 then
   echo "ERROR: Cannot use the 'items' keyword as a category"
   exit 5
@@ -127,20 +127,20 @@ fi
 
 # validating the items header line
 items=$(grep "[^[:space:]]" $1 | head -4 | tail -1)
-if [[ $(echo $items | grep "items:[0-9]+(,[0-9]+)*") == '' ]]
+if [[ $(echo -n $items | grep -E "items:[0-9]+(,[0-9]+)*$") == '' ]]
 then
-  if [[ $items == '' ]]
+  if [[ $items == $categories ]]
   then
     echo -e "ERROR: Missing header line\nLast header line: $categories"
   else
-    echo "ERROR: Invalid items header line, must be of the form 'items:<num>[,<num>]+'"
+    echo "ERROR: Invalid items header line, must be of the form 'items:<num>[,<num>]*'"
   fi
   exit 6
 fi
 
 # checking that the number of categories matches the number of items
-num_categories=$(echo $categories | awk -F, '{print NF}')
-num_items=$(echo $items | awk -F, '{print NF}')
+num_categories=$(echo -n $categories | awk -F, '{print NF}')
+num_items=$(echo -n $items | awk -F, '{print NF}')
 if [[ $num_categories != $num_items ]]
 then
   echo "ERROR: invalid item quantities: $num_categories categories but $num_items items"

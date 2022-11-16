93f4b8e32255eab9902774248c144f7d6f56167e
Bryant Collins <vitrial@vitrial.xyz>
2022-11-16 12:50:09 -0500

    added the nospaces version that works fine but has no spaces on items

---
+++ print-nospaces.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+if [[ $# > 2 || $# < 1 ]]
+then
+  echo "Usage: $0 <filename> -c"
+  exit 1
+fi
+
+# if -c is present sort by ascending order of total cost
+if [[ $# == 2 && $2 != "-c" ]]
+then
+  echo "Invalid flag"
+  exit 2
+fi
+
+if [[ $# == 2 && $2 == "-c" ]]
+then
+  numSort=1
+else
+  numSort=0
+fi
+
+lc=$(($(grep . $1 | wc -l) - 4))
+
+grep . $1 | tail -$lc > tmp.txt
+
+temp=""
+
+for i in $(grep . tmp.txt | sed 's/ //g')
+do
+  if [[ $numSort == 1 ]]
+  then
+    temp=$temp"$(echo $i | awk -F, '{$4=$2 * $3;printf "%.2f,%s,%.2f,%d", $4, $1, $2, $3}')\n"
+  else
+    temp=$temp"$(echo $i | awk -F, '{$4=$2 * $3;printf "%s,%.2f,%d,%.2f", $1, $2, $3, $4}')\n"
+  fi
+done
+
+if [[ $numSort == 1 ]]
+then
+  echo -e $temp | grep . | sort -n > tmp.txt
+  temp2=""
+  for i in $(cat tmp.txt)
+  do
+    temp2=$temp2"$(echo $i | awk -F, '{printf "%s,%.2f,%d,%.2f", $2, $3, $4, $1}')\n"
+  done
+  echo -e $temp2 | grep . > tmp.txt
+else
+  echo -e $temp | grep . | sort > tmp.txt
+fi
+
+
+#cat tmp.txt
+
+table="\\\documentclass{article}\n\\\pagestyle{empty}\n\\\begin{document}\n\\\begin{center}\n\\\begin{tabular}{|rrrrr|}\n\\\hline\n"
+table=$table"\multicolumn{5}{|c|}{$(grep . $1 | head -1 | awk -F: '{print $2}')}\\\\\\\\\n"
+table=$table"\multicolumn{5}{|c|}{$(grep . $1 | head -2 | tail -1 | awk -F: '{print $2}')}\\\\\\\\\n"
+table=$table"\\\hline\n"
+table=$table"Category&Item&Cost&Quantity&Total\\\\\\\\\n"
+table=$table"\\\hline\\\hline\n"
+
+i=1
+while [[ $i != $(($lc+1)) ]]
+do
+  line=$(grep . tmp.txt | head -$i | tail -1)
+  echo $line
+  table=$table"$(echo $line | awk -F: '{print $1}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $2}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $3}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $4}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $5}')\\\\\\\\\n"
+  i=$(($i+1))
+done
+
+table=$table"\\\hline\n"
+
+table=$table"\\\end{tabular}\n\\\end{center}\n\\\end{document}"
+
+echo -e $table > tmp.tex
+pdflatex tmp.tex  && rm tmp.aux tmp.log
+okular tmp.pdf
+
+rm tmp.txt tmp.tex tmp.pdf

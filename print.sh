fe44d686071a9a615fd7409b8b69cd9bd02681a1
Bryant Collins <vitrial@vitrial.xyz>
2022-11-16 17:02:46 -0500

    shit works now

--- print.sh
+++ print.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Author: Brayan Mauricio-Gonzalez & Bryant Collins
+# Assignment 3 - Print
+# Purpose: create a table showing info inside a provided file
+
 if [[ $# > 2 || $# < 1 ]]
 then
-  echo "Usage: $0 <filename> -c"
+  echo "Usage: $0 <filename> [-c]"
   exit 1
 fi
 
@@ -12,43 +17,21 @@ then
   exit 2
 fi
 
-if [[ $# == 2 && $2 == "-c" ]]
-then
-  numSort=1
-else
-  numSort=0
-fi
-
+# get linecount for easy use
 lc=$(($(grep . $1 | wc -l) - 4))
 
-grep . $1 | tail -$lc > tmp.txt
+# tack on the the total cost at the end of each line
+grep . $1 | tail -$lc | awk -F, '{$4=$2*$3;printf "%s,%.2f,%d,%.2f\n", $1, $2, $3, $4}' > tmp.txt
 
-temp=""
-
-for i in $(grep . tmp.txt | awk -F, '{printf "%s,%.2f,%d\n", $1, $2, $3}' )
-do
-  echo $i
-  if [[ $numSort == 1 ]]
-  then
-    temp=$temp"$(echo $i | awk -F, '{$4=$2 * $3;printf "%.2f,%s,%.2f,%d", $4, $1, $2, $3}')\n"
-  else
-    temp=$temp"$(echo $i | awk -F, '{$4=$2 * $3;printf "%s,%.2f,%d,%.2f", $1, $2, $3, $4}')\n"
-  fi
-done
-
-if [[ $numSort == 1 ]]
+# sort by either the category or total cost
+if [[ $# == 2 && $2 == "-c" ]]
 then
-  echo -e $temp | grep . | sort -n > tmp.txt
-  temp2=""
-  for i in $(cat tmp.txt)
-  do
-    temp2=$temp2"$(echo $i | awk -F, '{printf "%s,%.2f,%d,%.2f", $2, $3, $4, $1}')\n"
-  done
-  echo -e $temp2 | grep . > tmp.txt
+  sort tmp.txt -t, -b -n -k 4 -o tmp.txt
 else
-  echo -e $temp | grep . | sort > tmp.txt
+  sort tmp.txt -f -o tmp.txt
 fi
 
+# the basic beginning of the latex document
 table="\\\documentclass{article}\n\\\pagestyle{empty}\n\\\begin{document}\n\\\begin{center}\n\\\begin{tabular}{|rrrrr|}\n\\\hline\n"
 table=$table"\multicolumn{5}{|c|}{$(grep . $1 | head -1 | awk -F: '{print $2}')}\\\\\\\\\n"
 table=$table"\multicolumn{5}{|c|}{$(grep . $1 | head -2 | tail -1 | awk -F: '{print $2}')}\\\\\\\\\n"
@@ -56,24 +39,30 @@ table=$table"\\\hline\n"
 table=$table"Category&Item&Cost&Quantity&Total\\\\\\\\\n"
 table=$table"\\\hline\\\hline\n"
 
+# loop to build each line of the latex file
 i=1
 while [[ $i != $(($lc+1)) ]]
 do
   line=$(grep . tmp.txt | head -$i | tail -1)
   table=$table"$(echo $line | awk -F: '{print $1}')&"
-  table=$table"$(echo $line | awk -F'[:,]' '{print $2}' | sed 's/^ //')&"
-  table=$table"$(echo $line | awk -F'[:,]' '{print $3}' | sed 's/^ //')&"
-  table=$table"$(echo $line | awk -F'[:,]' '{print $4}' | sed 's/^ //')&"
-  table=$table"$(echo $line | awk -F'[:,]' '{print $5}' | sed 's/^ //')\\\\\\\\\n"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $2}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $3}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $4}')&"
+  table=$table"$(echo $line | awk -F'[:,]' '{print $5}')\\\\\\\\\n"
   i=$(($i+1))
 done
 
+# basic end of a latex file
 table=$table"\\\hline\n"
-
 table=$table"\\\end{tabular}\n\\\end{center}\n\\\end{document}"
 
+# copy stuff into a temp tex file, then compile and cleanup
+# piping into /dev/null cause I don't care about the compile messages
 echo -e $table > tmp.tex
-pdflatex tmp.tex &> /dev/null  && rm tmp.aux tmp.log
+pdflatex tmp.tex &> /dev/null && rm tmp.aux tmp.log
+
+# view file
 okular tmp.pdf
 
-#rm tmp.txt tmp.tex tmp.pdf
+# cleanup
+rm tmp.txt tmp.tex tmp.pdf
